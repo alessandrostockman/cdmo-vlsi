87f7b4933c210c5f4db40fc97ecfaaa5d0361dc4
Alessandro Stockman <alessandrostockman@gmail.com>
2021-06-14 22:31:18 +0200

    fix utils and add solution writing

--- lib/utils.py
+++ lib/utils.py
@@ -7,29 +7,38 @@ def load_instance(filename):
         circuit_num = int(sol.readline().strip())
         circuits = []
         for i in range(circuit_num):
-            circuits.append(tuple(int(el) for el in sol.readline().strip().split(" "))[::-1])
+            circuits.append(tuple(int(el) for el in sol.readline().strip().split(" ")))
         return width, circuits
 
 def load_solution(filename):
     with open(filename, 'r') as sol:
-        plate = tuple(int(el) for el in sol.readline().strip().split(" "))[::-1]
+        plate = tuple(int(el) for el in sol.readline().strip().split(" "))
         circuit_num = int(sol.readline().strip())
         circuits = []
         for i in range(circuit_num):
-            circuits.append(tuple(int(el) for el in sol.readline().strip().split(" "))[::-1])
+            circuits.append(tuple(int(el) for el in sol.readline().strip().split(" ")))
         return plate, circuits
 
 def plot_result(plate, circuits):
-    matrix = np.zeros(plate, dtype=int)
+    matrix = np.zeros(plate[::-1], dtype=int)
 
     fig, ax = plt.subplots()
-    for idx, (x, y, w, h) in enumerate(circuits):
+    for idx, (w, h, x, y) in enumerate(circuits):
         for i in range(w):
             for j in range(h):
-                matrix[x+i, y+j] = idx+1
+                matrix[y+j, x+i] = idx+1
                 # ax.text(y+j+0.5, x+i+0.5, str(idx+1), va='center', ha='center')
 
-    plt.pcolormesh(matrix)
-    ax.set_xticks(np.arange(plate[1]))
-    ax.set_yticks(np.arange(plate[0]))
+    plt.pcolormesh(matrix, edgecolors='black')
+    ax.set_xticks(np.arange(plate[0]))
+    ax.set_yticks(np.arange(plate[1]))
     plt.show()
+
+def write_solution(filename, solution):
+    with open(filename, 'w') as sol:
+        (plate_width, plate_height), circuits_pos = solution
+        sol.write("{0} {1}\n".format(plate_width, plate_height))
+        sol.write("{0}\n".format(len(circuits_pos)))
+        for c in circuits_pos:
+            w, h, x, y = c
+            sol.write("{0} {1} {2} {3}\n".format(w, h, x, y))
