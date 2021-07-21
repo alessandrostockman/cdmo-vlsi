7bc964f98ae92ce5fed2a4706d3bad38299fb193
Alessandro Stockman <alessandrostockman@gmail.com>
2021-07-21 12:09:44 +0200

    add cp solution status check

--- lib/solver.py
+++ lib/solver.py
@@ -1,4 +1,4 @@
-from minizinc import Instance, Model, Solver
+from minizinc import Instance, Model, Solver, Status
 
 def solve_cp(code_file, data):
     model = Model(code_file)
@@ -16,7 +16,10 @@ def solve_cp(code_file, data):
 
     result = instance.solve()
 
-    circuits_pos = [(w, h, x, y) for (w, h), x, y in zip(circuits, result["x"], result["y"])]
-    plate_height = result.objective
+    if result.status is Status.OPTIMAL_SOLUTION:
+        circuits_pos = [(w, h, x, y) for (w, h), x, y in zip(circuits, result["x"], result["y"])]
+        plate_height = result.objective
 
-    return (plate_width, plate_height), circuits_pos
+        return (plate_width, plate_height), circuits_pos
+    else:
+        return None
