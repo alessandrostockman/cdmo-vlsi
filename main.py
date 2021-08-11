f179a37d34e0a67af247d49538a838f2ff35f6db
Alessandro Stockman <alessandrostockman@gmail.com>
2021-08-11 11:04:33 +0200

    add arguments

--- main.py
+++ main.py
@@ -10,8 +10,11 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("-S", "--solver", default="null", help="", choices=["null", "cp", "sat"])
     parser.add_argument('-P', '--plot', help="", default=False, action='store_true')
+    parser.add_argument('-X', '--stats', help="", default=False, action='store_true')
+    parser.add_argument('-A', '--average', help="", default=False, action='store_true')
     parser.add_argument('-I', '--instances', help="", default="res/instances")
     parser.add_argument('-O', '--output', help="", default="res/solutions")
+    parser.add_argument('-T', '--timeout', help="", default=5*60)
     args = parser.parse_args()
 
     if os.path.isdir(args.instances):
@@ -22,6 +25,8 @@ if __name__ == "__main__":
         inputs = ["res/instances/ins-{0}.txt".format(i) for i in range(41)]
 
     solutions = []
+    stats_times = []
+    
     for input in inputs:
         filename = os.path.basename(input)
         output = os.path.join(args.output, filename)
@@ -34,17 +39,35 @@ if __name__ == "__main__":
                 continue
             sol = load_solution(output)
         else:
-            timeout = 5 * 60
-            if args.solver == "cp": 
-                sol, execution_time = solve_cp("cp/src/solver.mzn", instance, timeout)
-            if args.solver == "sat": 
-                sol, execution_time = solve_sat(instance, timeout)
-            print("Problem {0} solved in {1}s".format(input, round(execution_time, 4)))
+            first = True
+            times = []
+            sol = None
+            execution_time = None
+            while first or args.average and len(times) < 10:
+                first = False
+                if args.solver == "cp": 
+                    sol, execution_time = solve_cp("cp/src/solver.mzn", instance, int(args.timeout))
+                if args.solver == "sat": 
+                    sol, execution_time = solve_sat(instance, int(args.timeout))
+
+                if sol is not None:
+                    times.append(execution_time)
+                    if not args.average:
+                        print("Problem {0} solved in {1}s".format(input, round(execution_time, 4)))
+                else:
+                    print("Problem {0} not solved".format(input))
             
-            write_solution(output, sol)
+            if sol is not None:
+                if args.average:
+                    avg_time = sum(times) / len(times)
+                    print("Problem {0} solved in an average of {1}s".format(input, round(avg_time, 4)))
+                    stats_times.append(avg_time)
+                else:
+                    stats_times.append(execution_time)
+                write_solution(output, sol)
         
         solutions.append((sol, input))
-    
+
     if args.plot:
         for sol, title in solutions:
             plate, circuits_pos = sol
